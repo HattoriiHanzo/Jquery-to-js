7ec8e877408da5ea8aa63c8621f93ce58dc7587c
DiegoValdez <dvaldez.4g@gmail.com>
2019-04-23 00:11:07 +0200

    Viendo las promesas

--- src/js/home.js
+++ src/js/home.js
@@ -10,3 +10,42 @@ let cambia = "@DiegoValdez"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise (function(todoBien, todoMal){
+  //Llamar a una api
+  setTimeout(function(){
+    //Luego de 3 segundos ejecutará lo siguiente
+    todoBien('Se acabó el tiempo');
+  },5000)
+
+})
+
+const getUser = new Promise (function(todoBien, todoMal){
+  //Llamar a una api
+  setTimeout(function(){
+    //Luego de 3 segundos ejecutará lo siguiente
+    todoBien('Se acabó el tiempo 2.0');
+  },3000)
+
+})
+
+// getUser
+//   .then(function(){
+//     console.log('Todo está bien en la vida')
+//   })
+//   .catch(function(message){
+//     console.log(message)
+//   })
+
+// si se pone Promise.race solo se visualizará la promesa que se resulva antes, este caso la segunda, la 2.0
+// ya que tarda 3 segundos y no 5
+Promise.all([
+  getUser,
+  getUserAll
+])
+  .then(function(message){
+    console.log(message)
+  })
+  .catch(function(message){
+    console.log(message)
+  })
